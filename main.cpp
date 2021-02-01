e88c9f417d1a78df362453b3f65240c058691f42
d.ivakhnenko <d.ivakhnenko@ddos-guard.net>
2021-02-01 21:25:32 +0300

    parsing of line arguments added

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
 
 #include "uinterface.h"
 
 
+std::string keys =
+        "{ help  h     | | Print help message. }"
+        "{@path        | | Path to directory with images. }";
+
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cout << "usage: ImageViewer <path_to_directory>" << std::endl;
-        return -1;
+    cv::CommandLineParser parser(argc, argv, keys);
+    parser = cv::CommandLineParser(argc, argv, keys);
+
+    parser.about("Viewing of images from directory.");
+
+    if (argc == 1 || parser.has("help")) {
+        parser.printMessage();
+        return 0;
     }
 
     int window_width = 600;
     int window_height = 800;
 
-    switch (UInterface(argv[1], window_width, window_height).start()) {
+    switch (UInterface(parser.get<std::string>(0), window_width, window_height).start()) {
         case NO_IMAGES:
             std::cout << "target directory doesn't have images" << std::endl;
             return -2;
